02c6d05b6b60be3ee546b19d3ebc59635dc5ec38
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-11-30 13:37:08 -0600

    removed MainPage

--- src/AllProducts/index.js
+++ src/AllProducts/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Icon } from "semantic-ui-react";
 
 class AllProducts extends React.Component {
   constructor() {
@@ -7,7 +8,29 @@ class AllProducts extends React.Component {
     this.state = {};
   }
   render() {
-    return "welcome to the AllProducts page";
+    return (
+      //   <div>
+      //     <h3>ecommerce site</h3>
+      //     <div className="top">
+      //       <Button primary>Wish List</Button>
+      //       <Button primary>My Account</Button>
+      //     </div>
+      //     <Icon name="lock" size="huge" />
+      //     <div className="nav">
+      //       <Button>
+      //         <Icon name="shopping cart" />
+      //       </Button>
+      //       <Button>
+      //         <Icon name="search" />
+      //       </Button>
+      //       <Button primary onClick={this.showAllProducts}>
+      //         Products
+      //       </Button>
+      //       <Button primary>Home</Button>
+      //     </div>
+      //   </div>
+      "you are hitting the products page"
+    );
   }
 }
 
--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React from "react";
-import MainPage from "./MainPage";
 import "./App.css";
+import { Button, Icon } from "semantic-ui-react";
+import iphone11 from "./iphone11Pro.png";
+import AllProducts from "./AllProducts";
 
 class App extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      guest: true,
+      showingProducts: false,
+      loggedIn: false
+    };
   }
   render() {
     return (
       <div>
-        <MainPage />
+        {this.state.showingProducts ? <AllProducts /> : null}
+        <h3>ecommerce site</h3>
+        <div className="top">
+          <Button secondary>Wish List</Button>
+          <Button secondary onClick={this.showMyAccount}>
+            My Account
+          </Button>
+        </div>
+        <Icon name="lock" size="huge" />
+        <div className="nav">
+          <Button>
+            <Icon name="shopping cart" />
+          </Button>
+          <Button>
+            <Icon name="search" />
+          </Button>
+          <Button secondary onClick={this.showAllProducts}>
+            Products
+          </Button>
+          <Button secondary>Home</Button>
+        </div>
+        <div className="img">
+          <img src={iphone11} alt="Iphone 11 Pro Max" />
+          <Icon name="right arrow" size="huge" className="arrow" />
+        </div>
       </div>
     );
   }
