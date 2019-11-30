347f3c61b17acc75c50684c09d3f3a34215c4a14
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-11-30 15:42:58 -0600

    included dropdown into myAccount button

--- src/AllProducts/index.js
+++ src/AllProducts/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Icon } from "semantic-ui-react";
+// import { Button, Icon } from "semantic-ui-react";
 
 class AllProducts extends React.Component {
   constructor() {
@@ -8,29 +8,7 @@ class AllProducts extends React.Component {
     this.state = {};
   }
   render() {
-    return (
-      //   <div>
-      //     <h3>ecommerce site</h3>
-      //     <div className="top">
-      //       <Button primary>Wish List</Button>
-      //       <Button primary>My Account</Button>
-      //     </div>
-      //     <Icon name="lock" size="huge" />
-      //     <div className="nav">
-      //       <Button>
-      //         <Icon name="shopping cart" />
-      //       </Button>
-      //       <Button>
-      //         <Icon name="search" />
-      //       </Button>
-      //       <Button primary onClick={this.showAllProducts}>
-      //         Products
-      //       </Button>
-      //       <Button primary>Home</Button>
-      //     </div>
-      //   </div>
-      "you are hitting the products page"
-    );
+    return "all products index page";
   }
 }
 
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Button, Icon } from "semantic-ui-react";
+import { Button, Icon, Menu, Dropdown } from "semantic-ui-react";
 import iphone11 from "./iphone11Pro.png";
 import AllProducts from "./AllProducts";
 
@@ -11,21 +11,41 @@ class App extends React.Component {
     this.state = {
       guest: true,
       showingProducts: false,
-      loggedIn: false
+      loggedIn: false,
+      options: [
+        { key: 1, text: "Sign Up", value: 1 },
+        { key: 2, text: "Login", value: 2 }
+      ]
     };
   }
+
+  // create a method that will allow the user to see the products page
+  showProducts = () => {
+    console.log("you are hitting the products page");
+    this.setState({
+      showingProducts: true
+    });
+  };
+
+  // create a method that will show the myAccount Component
+
   render() {
     return (
       <div>
-        {this.state.showingProducts ? <AllProducts /> : null}
         <h3>ecommerce site</h3>
         <div className="top">
           <Button secondary>Wish List</Button>
-          <Button secondary onClick={this.showMyAccount}>
-            My Account
-          </Button>
+          <Menu compact>
+            {/* <Button secondary>My Account</Button> */}
+            <Dropdown
+              text="My Account"
+              options={this.state.options}
+              simple
+              item
+            />
+          </Menu>
         </div>
-        <Icon name="lock" size="huge" />
+        <p>logo</p>
         <div className="nav">
           <Button>
             <Icon name="shopping cart" />
@@ -33,14 +53,16 @@ class App extends React.Component {
           <Button>
             <Icon name="search" />
           </Button>
-          <Button secondary onClick={this.showAllProducts}>
+          <Button secondary onClick={this.showProducts}>
             Products
           </Button>
           <Button secondary>Home</Button>
         </div>
+        {/* {this.state.showingProducts ? <AllProducts /> : null} */}
         <div className="img">
+          <h1>Shop Now</h1>
           <img src={iphone11} alt="Iphone 11 Pro Max" />
-          <Icon name="right arrow" size="huge" className="arrow" />
+          <Icon link name="right arrow" size="big" className="arrow" />
         </div>
       </div>
     );
