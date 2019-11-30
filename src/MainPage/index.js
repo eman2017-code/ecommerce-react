c564d205a4060438bf0e7e5a3ddccca0f207ce00
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-11-30 11:46:54 -0600

    imported main image into mainPage

--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -1,26 +1,39 @@
 import React from "react";
 import { Button, Icon } from "semantic-ui-react";
+import iphone11 from "../iphone11Pro.png";
 
 class MainPage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      guest: true
+    };
   }
 
   render() {
     return (
       <div>
-        <div>
+        <div className="top">
           <h3>ecommerce site</h3>
-          <p className="icons">shopping cart icon here</p>
+          <Button primary>Wish List</Button>
+          <Button primary>My Account</Button>
         </div>
-        <div className="header">
+        <p className="logo">logo</p>
+        <div className="leftSide">
+          <Button>
+            <Button.Content visible>Search Icon</Button.Content>
+            <Icon name="search" />
+          </Button>
+          <Button>
+            <Button.Content visible>Shopping Cart</Button.Content>
+            <Icon name="shopping cart" />
+          </Button>
+          <Button primary>Products</Button>
           <Button primary>Home</Button>
-          <Button secondary>Products</Button>
         </div>
         <div>
-          <p>insert the main image here</p>
+          <img src={iphone11} alt="Iphone 11 Pro Max" />
         </div>
       </div>
     );
