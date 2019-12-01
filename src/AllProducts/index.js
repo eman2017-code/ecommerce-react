8ab1c30ed7f8b2abd3b1f0fb5a9bc23f0015875d
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-12-01 13:26:02 -0600

    router now renders products page component

---
+++ src/AllProducts/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AllProducts extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+  render() {
+    return <h1>this will be a list of all the products</h1>;
+  }
+}
+
+export default AllProducts;
--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import Route from "react-router-dom/Route";
 import { Button, Icon } from "semantic-ui-react";
 import iphone11 from "./iphone11Pro.png";
 import SearchProducts from "./SearchProducts";
+import AllProducts from "./AllProducts";
 
 class App extends React.Component {
   constructor() {
@@ -18,27 +21,51 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <h3>ecommerce site</h3>
-        <div className="top">
-          <Button secondary>Wish List</Button>
-          <Button secondary>My Account</Button>
+      <Router>
+        <div>
+          <Route
+            path="/home"
+            exact
+            strict
+            render={() => {
+              return (
+                <div>
+                  <h3>ecommerce site</h3>
+                  <div className="top">
+                    <Button secondary>Wish List</Button>
+                    <Button secondary>My Account</Button>
+                  </div>
+                  <p>logo</p>
+                  <div className="nav">
+                    <Button>
+                      <Icon name="shopping cart" />
+                    </Button>
+                    <SearchProducts />
+
+                    <Link to="/home">
+                      <Button secondary>Home</Button>
+                    </Link>
+                    <Link to="/products">
+                      <Button secondary>Products</Button>
+                    </Link>
+                  </div>
+                  <div className="img">
+                    <h1>Shop Now</h1>
+                    <img src={iphone11} alt="Iphone 11 Pro Max" />
+                    <Icon
+                      link
+                      name="right arrow"
+                      size="big"
+                      className="arrow"
+                    />
+                  </div>
+                </div>
+              );
+            }}
+          />
+          <Route path="/products" exact strict component={AllProducts} />
         </div>
-        <p>logo</p>
-        <div className="nav">
-          <Button>
-            <Icon name="shopping cart" />
-          </Button>
-          <SearchProducts />
-          <Button secondary>Products</Button>
-          <Button secondary>Home</Button>
-        </div>
-        <div className="img">
-          <h1>Shop Now</h1>
-          <img src={iphone11} alt="Iphone 11 Pro Max" />
-          <Icon link name="right arrow" size="big" className="arrow" />
-        </div>
-      </div>
+      </Router>
     );
   }
 }
