bc7c5c0ec50eadc55cf64d98cb9c90f3d35cf2c1
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-12-01 12:09:09 -0600

    removed AllProducts/UserShowMyAccount pagers

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import { Button, Icon } from "semantic-ui-react";
 import iphone11 from "./iphone11Pro.png";
-import AllProducts from "./AllProducts";
 import UserShowMyAccount from "./UserShowMyAccount";
 
 class App extends React.Component {
@@ -11,18 +10,9 @@ class App extends React.Component {
 
     this.state = {
       guest: true,
-      showingProducts: false,
-      loggedIn: false,
-      showAccount: false
+      loggedIn: false
     };
   }
-
-  // create a method that will allow the user to see the products page
-  showProducts = () => {
-    this.setState({
-      showingProducts: true
-    });
-  };
 
   // create a method that will show the myAccount component
   showMyAccount = () => {
@@ -49,13 +39,9 @@ class App extends React.Component {
           <Button>
             <Icon name="search" />
           </Button>
-          <Button secondary onClick={this.showProducts}>
-            Products
-          </Button>
+          <Button secondary>Products</Button>
           <Button secondary>Home</Button>
         </div>
-        {/* {this.state.showingProducts ? <AllProducts /> : null} */}
-        {/* {this.state.showAccount ? <UserShowMyAccount /> : null} */}
         <div className="img">
           <h1>Shop Now</h1>
           <img src={iphone11} alt="Iphone 11 Pro Max" />
--- src/UserShowMyAccount/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function UserShowMyAccount() {
-  return "welcome to the UserShowMyAccount page";
-}
-
-export default UserShowMyAccount;
