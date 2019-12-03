bb016e8325aff4cdfbc6bc3286b57903f88212f6
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-12-03 08:35:47 -0600

    including header component - making site more modular

---
+++ src/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Header extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return "hey";
+  }
+}
+
+export default Header;
