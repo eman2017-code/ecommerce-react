9dc477646da8c2f1817bca5aba2966f36381351c
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-12-05 17:27:28 -0600

    user can see images when posted

--- src/ListProducts/index.js
+++ src/ListProducts/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Card, Button } from "semantic-ui-react";
+import { Card, Button, Image } from "semantic-ui-react";
 
 function ListProducts(props) {
   const products = props.products.map(product => {
     return (
       <Card key={product.id}>
+        <Image src={product.picture} />
         <Card.Content>
           <Card.Header>{product.name}</Card.Header>
           <Card.Description>{product.description}</Card.Description>
