ff8535e4b6b338cfc756f7eb89134a619c3369ec
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-12-11 16:19:56 -0600

    updated styling to main page

--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -1,9 +1,19 @@
 import React from "react";
 import Nav from "../Nav";
 import iphone11 from "../iphone11Pro.png";
-// import iphone11ProMax from "../iphone11ProMax.jpg";
+import applwatch from "../applwatch.jpg";
 import airpods from "../airPodsCase.jpeg";
 import yeezy from "../yeezy.jpg";
+import appleTvRemote from "../appleTvRemote.jpeg";
+import samsungFridge from "../samsungFridge.webp";
+import printer from "../printer.jpg";
+import yeezys from "../yeezys.jpg";
+import supremeHoodie from "../supremeHoodie.jpg";
+import iphoneX from "../iphoneX.jpg";
+import beats from "../beats.jpeg";
+import bike from "../bike.jpg";
+import samsungHeadphones from "../samsungHeadphones.jpg";
+import oculus from "../oculus.jpg";
 
 class MainPage extends React.Component {
   constructor(props) {
@@ -24,12 +34,27 @@ class MainPage extends React.Component {
         </div>
         <div className="middleImgDiv">
           <img src={iphone11} alt="Iphone 11 Pro Max" className="middleImg" />
-          <img src={iphone11} alt="Iphone 11 Pro Max" className="middleImg" />
-          <img src={iphone11} alt="Iphone 11 Pro Max" className="middleImg" />
+          <img
+            src={samsungHeadphones}
+            alt="samsung headphones"
+            className="middleImg"
+          />
+          <img src={oculus} alt="VR Headset" className="middleImg" />
         </div>
-        <h3>Trending Now</h3>
-        <div className="lastImgDiv">
+        <h4 className="newCollection">NEW COLLECTION</h4>
+        <h1 className="trending">TRENDING PRODUCTS</h1>
+        {/* <div className="lastImgDiv">
           <img src={yeezy} alt="yeezy" className="lastImg" />
+        </div> */}
+        <div className="lastImgDiv">
+          <img src={appleTvRemote} alt="apple tv remote" className="lastImg" />
+          <img src={samsungFridge} alt="samsung fridge" className="lastImg" />
+          <img src={applwatch} alt="apple watch" className="lastImg" />
+          <img src={supremeHoodie} alt="supreme hoodie" className="lastImg" />
+          <img src={iphoneX} alt="Iphone X" className="lastImg" />
+          <img src={yeezys} alt="yeezys" className="lastImg" />
+          <img src={beats} alt="beats headphones" className="lastImg" />
+          <img src={bike} alt="bicycle" className="lastImg" />
         </div>
       </div>
     );
