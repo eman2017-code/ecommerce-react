c12960e47663ac54f9ba5b662602180a1268dfd1
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-12-16 21:16:36 -0600

    updated the responsivness for admin dashboard

--- src/AdminDashboard/index.js
+++ src/AdminDashboard/index.js
@@ -163,11 +163,6 @@ export default function AdminDashboard(props) {
             <Grid item xs={12} md={8} lg={9}>
               <Paper className={fixedHeightPaper}>{<Chart />}</Paper>
             </Grid>
-            {/* Recent Deposits */}
-            <Grid item xs={12} md={4} lg={3}>
-              <Paper className={fixedHeightPaper}>{/* <Deposits /> */}</Paper>
-            </Grid>
-            {/* Recent Orders */}
             <SearchProducts />
             <Grid item xs={12}>
               <Paper className={classes.paper}>
